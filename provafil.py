86ecbd680e720e5a9f53476a7dd34c8b26b43980
filippogiorgiorondo <filippogiorgior@gmail.com>
2025-05-05 14:36:11 +0200

    update

--- provafil.py
+++ provafil.py
@@ -5,9 +5,22 @@ import missingno as msno
 
 amazon = pd.read_csv("C:/Users/filip/Documents/Analisi-Amazon-FBA/Amazon Sale Report.csv")
 # === Data Cleaning ===
-def clean_data(df):
-    df = df.drop_duplicates()
-    return df
+def clean_data(amazon):
+    # Eliminazione delle colonne inutili
+    amazon.drop(columns = ['Unnamed: 22','fulfilled-by','ship-country', 'currency'], inplace = True)
+    
+    # Eliminazione delle righe duplicate
+    before_remove_duplicates = len(amazon)
+    amazon.drop_duplicates(inplace = True)
+    after_remove_duplicates = len(amazon)
+    duplicate_rows_removed = before_remove_duplicates - after_remove_duplicates
+    print(f'{duplicate_rows_removed} duplicate rows have been removed! \nThe Dataset now has {after_remove_duplicates} rows.')
+
+    # Restituisce le righe con i dati mancanti
+    print("Righe con dati mancanti:\n")
+    amazon[amazon.isnull().any(axis = 1)]
+
+    return amazon
 
 # imposta una colonna chiamata 'index' come indice del DataFrame amazon.
 amazon.set_index('index', inplace = True)
@@ -25,3 +38,8 @@ plt.show()
 
 print(amazon.nunique())
 print(amazon.apply(pd.unique))
+
+print(amazon[amazon.isnull().any(axis = 1)])
+
+amazon = clean_data(amazon)
+print(amazon)
